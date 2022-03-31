4686d7249bb958db7a2db6f99c8869b7ec3b63d1
Saida-Mahmudova <mahmudovas.au@gmail.com>
2022-03-31 20:23:02 +0300

    function for validation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,17 @@
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
+const createTweetElement = (tweet) => {
+  const article = $('<article>').addClass('tweet-section');
+  const avatar = $(`<img src =${tweet.user.avatars}>`)
+  const username = $('<p>').text(`${tweet.user.name}`)
+  const userDetails = $('<div>').addClass('username').append(avatar, username)
+  const handle = $('<p>').text(`${tweet.user.handle}`).addClass('handle');
+  const head = $('<header>').addClass("name").append(userDetails, handle);
+  const text = $('<p>').text(`${tweet.content.text}`).addClass('message');
+  const date = $('</div><p>').text(`${timeago.format(tweet.created_at)}`)
+  const icons = $('<div><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i>').addClass('icons')
+  const footer = $('<footer>').addClass('tweet-details').append(date, icons);
+  const $tweet = article.append(head, text, footer);
+  return $tweet;
+};
 
 const renderTweets = (tweets) => {
   for (let tweet of tweets) {
@@ -40,36 +29,38 @@ const loadTweets = () => {
   })
 }
 
-const createTweetElement = (tweet) => {
-  const article = $('<article>').addClass('tweet-section');
-  const avatar = $(`<img src =${tweet.user.avatars}>`)
-  const username = $('<p>').text(`${tweet.user.name}`)
-  const userDetails = $('<div>').addClass('username').append(avatar, username)
-  const handle = $('<p>').text(`${tweet.user.handle}`).addClass('handle');
-  const head = $('<header>').addClass("name").append(userDetails, handle);
-  const text = $('<p>').text(`${tweet.content.text}`).addClass('message');
-  const date = $('</div><p>').text(`${timeago.format(tweet.created_at)}`)
-  const icons = $('<div><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i>').addClass('icons')
-  const footer = $('<footer>').addClass('tweet-details').append(date, icons);
-  const $tweet = article.append(head, text, footer);
-  return $tweet;
+
+const validate = (tweet) => {
+  let result;
+  if (tweet === '') {
+    result = false;
+    alert("Error: Tweet cannot be empty!");
+  } else if (tweet.length > 140) {
+    result = false;
+    alert("Error: Tweet is too long!");
+  } else {
+    result = true;
+  }
+  return result;
 };
 
 $(() => {
-  loadTweets();
 
   $('.tweet-form').on('submit', (e) => {
     e.preventDefault();
-    $('#submit-button').prop("disabled", true).text("Loading");
-    $.ajax({
-      type: "POST",
-      url: "/tweets",
-      data: $('.tweet-form').serialize()
-    }).then(() => {
-      $('#submit-button').prop("disabled", false).text("TWEET");
-      loadTweets();
-      $('#tweet-text').val('');
-      $('#tweet-text').focus();
-    });
+    if (validate($('#tweet-text').val()) === true) {
+      $('#submit-button').prop("disabled", true).text("Loading");
+      $.ajax({
+        type: "POST",
+        url: "/tweets",
+        data: $('.tweet-form').serialize()
+      }).then(() => {
+        $('#submit-button').prop("disabled", false).text("Tweet");
+        loadTweets();
+        $('#tweet-text').val('');
+        $('#tweet-text').focus();
+      });
+    }
   });
+  loadTweets();
 });
